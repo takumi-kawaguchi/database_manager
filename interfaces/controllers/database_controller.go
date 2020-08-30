68865ed19cdf402b263853356e3747b210111a42
Takumi Kawaguchi <tcgds4@gmail.com>
2020-08-30 20:07:36 +0900

    db一覧ページ作成

--- interfaces/controllers/database_controller.go
+++ interfaces/controllers/database_controller.go
@@ -4,13 +4,17 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/takumi-kawaguchi/database_manager/domain"
+	"github.com/takumi-kawaguchi/database_manager/infrastructure"
 )
 
 // GetDatabases returns all databases list
 func GetDatabases(c echo.Context) (err error) {
-	// db := infrastructure.DBConnect()
-	// defer db.Close()
-	// var databases []domain.Database
-	// db.Find(&databases)
-	return c.Render(http.StatusOK, "databases.tmpl", "databases")
+	db := infrastructure.DBConnect()
+	defer db.Close()
+	var databases []domain.Database
+	db.Find(&databases)
+	return c.Render(http.StatusOK, "databases.tmpl", echo.Map{
+		"databases": databases,
+	})
 }
